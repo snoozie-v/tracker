1fdde898663fca328400ced54497166a8ffd1433
snoozie-v <gettingsleepyz@gmail.com>
2023-09-29 21:11:02 -0700

    the mutant images have returned

--- src/App.jsx
+++ src/App.jsx
@@ -18,15 +18,12 @@ const connex = new Connex({
   network: "main"
 });
 
-//
-
-
 const startDateTimeString = "9/24/23 9:30 PM UTC";
-const startTimeStamp = Date.parse(startDateTimeString) / 1000; // Divide by 1000 to get seconds
+const startTimeStamp = Date.parse(startDateTimeString) / 1000; 
 
-// Replace this with the desired end date and time in UTC
+
 const endDateTimeString = "10/15/23 9:30 PM UTC";
-const endTimeStamp = Date.parse(endDateTimeString) / 1000; // Divide by 1000 to get seconds
+const endTimeStamp = Date.parse(endDateTimeString) / 1000;
 
 function getAccountForContract(contractAddress) {
   return contractToAccount[contractAddress] || null;
@@ -59,17 +56,17 @@ async function getImageForCollection(account, tokenId) {
       return presentImageURL;
 
     case "0x523bef286ac6b08eb1a9db765970852b086903fa":
-      // nftURI = account.method(tokenURIAbi);
-      // URIOutput = await nftURI.call(tokenId);
+      nftURI = account.method(tokenURIAbi);
+      URIOutput = await nftURI.call(tokenId);
 
-      // metadataResponse = await fetch(
-      //   `https://ipfs.io/ipfs/${URIOutput.decoded[0].substr(7)}`
-      // );
-      // metadata = await metadataResponse.json();
-      // imageUrl = metadata.image;
-      // presentImage = await fetch(`https://ipfs.io/ipfs/${imageUrl.substr(7)}`);
-      // presentImageURL = presentImage.url;
-      presentImageURL = mutants;
+      metadataResponse = await fetch(
+        `https://ipfs.io/ipfs/${URIOutput.decoded[0].substr(7)}`
+      );
+      metadata = await metadataResponse.json();
+      imageUrl = metadata.image;
+      presentImage = await fetch(`https://ipfs.io/ipfs/${imageUrl.substr(7)}`);
+      presentImageURL = presentImage.url;
+      // presentImageURL = mutants;
       return presentImageURL;
 
     case "0xc766ddd21f14862ef426f15bfb28573fdad8bc51":
@@ -190,7 +187,7 @@ export default function App() {
           })
           .order("desc")
           .apply(0, 200);
-          console.log(logs)
+
         const splitHexData = (hexData, numParts) => {
           const cleanHex = hexData.startsWith("0x")
             ? hexData.slice(2)
@@ -400,13 +397,13 @@ export default function App() {
             })();
 
             const buyer = getBuyer(decodedLog);
-            console.log("buyer", buyer)
+
 
             const nickName = await connex.thor
               .account("0xc7592f90A6746E5D55e4a1543b6caE6D5b11d258")
               .method(ABIWoVGetAccountProperties)
               .call(buyer)
-              console.log("nickname", nickName.decoded[4])
+
 
             const tokenId = getTokenId(decodedLog);
             const price = await getPrice(decodedLog);
